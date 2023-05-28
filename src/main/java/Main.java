64c9c15866afafd166e97ae7cd4437df585883ca
vadim novitskyi <thrownovitskyi@gmail.com>
2023-05-28 22:14:48 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+
+import java.net.http.HttpClient;
+
+@Data
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/TimeServlet.java
@@ -0,0 +1,41 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter("/time")
+public class TimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().write("<table>\n" +
+                "<tr>\n" +
+                "<td>Date</td>\n" +
+                "<td>Time</td>\n" +
+                "<td>Time zone</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td>12</td>\n" +
+                "<td>12</td>\n" +
+                "<td>zone</td>\n" +
+                "</tr>\n" +
+                "</table>");
+        resp.getWriter().close();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("Servlet created");
+    }
+}
